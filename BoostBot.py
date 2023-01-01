129e0b433efbd576d430ac3fdc35b539b0b973be
Adam.#0005 <121742219+Adam0005@users.noreply.github.com>
2022-12-31 23:37:08 -0600

    Update BoostBot.py

--- BoostBot.py
+++ BoostBot.py
@@ -1,1 +1,20 @@
+import discord
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+async def boost(ctx):
+  # check if the user has the "Boost Server" permission
+  if ctx.author.guild_permissions.boost_server:
+    # check if the user has already boosted the server
+    if ctx.guild.premium_subscription_count > 0:
+      await ctx.send("You've already boosted the server. Thanks for your support!")
+    else:
+      # boost the server
+      await ctx.guild.premium_subscribe()
+      await ctx.send("Thanks for boosting the server! You now have access to additional features and perks.")
+  else:
+    await ctx.send("Sorry, you don't have permission to boost the server. Only users with the "Boost Server" permission can use this command.")
+
+bot.run('your-bot-token-here') 
